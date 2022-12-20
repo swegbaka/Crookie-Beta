dce6a7408bfd74e0335c4dcb7c3392a09dcb85cf
Jinwei Shao <78552537+swegbaka@users.noreply.github.com>
2022-12-19 19:38:13 -0500

    fixed problem where only shows only one url

--- test/js/DisplayURL.js
+++ test/js/DisplayURL.js
@@ -6,16 +6,29 @@ let b = 1;
 var meatButtons = document.querySelectorAll(".meat-butn-on");
 var foodNames = document.querySelectorAll(".testing");
 
-// const food = Beef({}, { _id: 0, foodName: 0, Link: 1, emoji: 0 });
-
 // assign each button using className
-meatButtons.forEach((t, i) => {
-  t.addEventListener("click", function () {
+for (let i = 0; i < meatButtons.length; i++) {
+  meatButtons[i].onclick = function () {
     b++;
     if (b % 2 == 0) {
-      foodNames[i].style.display = "block";
+      foodNames[i * 2].style.display = "block";
+      foodNames[i * 2 + 1].style.display = "block";
     } else {
-      foodNames[i].style.display = "none";
+      foodNames[i * 2].style.display = "none";
+      foodNames[i * 2 + 1].style.display = "none";
     }
-  });
-});
+  };
+}
+
+// meatButtons.forEach((t, i) => {
+//   t.addEventListener("click", function () {
+//     b++;
+//     if (b % 2 == 0) {
+//       foodNames[i * 2].style.display = "block";
+//       foodNames[i * 2 + 1].style.display = "block";
+//     } else {
+//       foodNames[i * 2].style.display = "none";
+//       foodNames[i * 2 + 1].style.display = "none";
+//     }
+//   });
+// });
